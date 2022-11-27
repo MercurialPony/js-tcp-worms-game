3bac23d0802b7919683d45bb55486fd3776a556b
edulyan <edgarkhurshudyan17@gmail.com>
2022-11-27 21:59:55 +0300

    timer animation - edited, add ellipsis animation

--- app/public/await-room/await-room.js
+++ app/public/await-room/await-room.js
@@ -1,34 +1,33 @@
 const userList = document.getElementById("user-list");
 
-function addPlayer(name)
-{
-	const li = document.createElement("li");
-	li.innerText = name;
-	userList.appendChild(li);
+function addPlayer(name) {
+  const li = document.createElement("li");
+  li.innerText = name;
+  userList.appendChild(li);
 }
 
-function removePlayer(name)
-{
-	Array.from(userList.getElementsByTagName("li"))
-	.filter(e => e.innerText === name)
-	.forEach(e => userList.removeChild(e));
+function removePlayer(name) {
+  Array.from(userList.getElementsByTagName("li"))
+    .filter((e) => e.innerText === name)
+    .forEach((e) => userList.removeChild(e));
 }
 
 var handler = parent.provideHandler();
 
-handler.json(0, data =>
-{
-	data.players.forEach(name => addPlayer(name));
+handler.json(0, (data) => {
+  data.players.forEach((name) => addPlayer(name));
 });
 
-handler.json(1, data =>
-{
-	const func = data.joined ? addPlayer : removePlayer;
-	func(data.username);
+handler.json(1, (data) => {
+  const func = data.joined ? addPlayer : removePlayer;
+  func(data.username);
 });
 
-handler.json(2, data =>
-{
-	timeLimit = timeLeft = data.timeToStart / 1000;
-	timePassed = 0;
+handler.json(2, (data) => {
+  document.getElementById("timer").removeAttribute("style");
+  document.getElementById("await-title").remove();
+  let user_list = document.getElementById("user-list-id");
+  user_list.style.marginTop = "50px";
+  timeLimit = timeLeft = data.timeToStart / 1000;
+  timePassed = 0;
 });

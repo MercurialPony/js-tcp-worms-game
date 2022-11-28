eaa52f8b8d37330e037044ffbcc72985b344bc45
edulyan <edgarkhurshudyan17@gmail.com>
2022-11-29 01:50:22 +0300

    edit input-ip, add dialog-table (not done)

--- app/public/connection/connection.js
+++ app/public/connection/connection.js
@@ -2,9 +2,32 @@ const ipInput = document.getElementById("ip");
 const portInput = document.getElementById("port");
 const userInput = document.getElementById("username");
 
+const dialog = document.getElementById("dialog-id");
+const openButton = document.getElementById("choose-ip");
+// const homeButton = dialog.querySelector('sl-button[slot="footer"]');
+// const closeButton = dialog.querySelector('sl-button[slot="footer"]');
+
+if (openButton) {
+  openButton.addEventListener("click", () => {
+    dialog.show();
+  });
+}
+
+// if (closeButton) {
+//   closeButton.addEventListener("click", () => dialog.hide());
+// }
+
+dialog.addEventListener("sl-request-close", (event) => {
+  if (event.detail.source === "overlay") {
+    event.preventDefault();
+  }
+});
+
 if (sessionStorage.getItem("username")) {
   userInput.setAttribute("value", sessionStorage.getItem("username"));
 }
+
+/////////////////////////////////////
 
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => e.preventDefault());

e889948fbeb47b933a418a485e3e2b46eca1fca8
edulyan <edgarkhurshudyan17@gmail.com>
2022-11-28 18:22:55 +0300

    add name existence error

--- app/public/connection/connection.js
+++ app/public/connection/connection.js
@@ -3,18 +3,19 @@ const portInput = document.getElementById("port");
 const userInput = document.getElementById("username");
 
 const form = document.getElementById("form");
-form.addEventListener("submit", e => e.preventDefault());
-form.addEventListener("submit", () => parent.connect(ipInput.value, portInput.value, userInput.value));
+form.addEventListener("submit", (e) => e.preventDefault());
+form.addEventListener("submit", () =>
+  parent.connect(ipInput.value, portInput.value, userInput.value)
+);
 
 var handler = parent.provideHandler();
 
-handler.json(0, data =>
-{
-	if(data.accepted)
-	{
-		window.location.href = "await-room/await-room.html";
-		return;
-	}
+handler.json(0, (data) => {
+  if (data.accepted) {
+    window.location.href = "await-room/await-room.html";
+    return;
+  }
 
-	// highlight error
+  alert(`This name "${userInput.value}" already exists`);
+  // highlight error
 });

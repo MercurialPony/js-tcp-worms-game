df3fc5e8aeac4340d67bde6314f7208656bd9b76
edulyan <edgarkhurshudyan17@gmail.com>
2022-11-28 21:18:47 +0300

    add save username

--- app/public/connection/connection.js
+++ app/public/connection/connection.js
@@ -2,11 +2,17 @@ const ipInput = document.getElementById("ip");
 const portInput = document.getElementById("port");
 const userInput = document.getElementById("username");
 
+if (sessionStorage.getItem("username")) {
+  userInput.setAttribute("value", sessionStorage.getItem("username"));
+}
+
 const form = document.getElementById("form");
 form.addEventListener("submit", (e) => e.preventDefault());
-form.addEventListener("submit", () =>
-  parent.connect(ipInput.value, portInput.value, userInput.value)
-);
+form.addEventListener("submit", () => {
+  parent.connect(ipInput.value, portInput.value, userInput.value);
+
+  sessionStorage.setItem("username", userInput.value);
+});
 
 var handler = parent.provideHandler();
 

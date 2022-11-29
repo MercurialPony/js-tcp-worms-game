42ce4fd47ac1313d76c7abf6a333eabe0570cc0e
edulyan <edgarkhurshudyan17@gmail.com>
2022-11-29 18:52:41 +0300

    test

--- app/public/main.js
+++ app/public/main.js
@@ -5,25 +5,18 @@ const MessageHandler = require("./message-handler");
 const BroadcastAddresses = require("./broadcast-addresses");
 const PNG = require("pngjs").PNG;
 
-
-
 /*------ Frame handling ------*/
 
-function getFrame()
-{
-	return document.getElementById("mainFrame").contentWindow;
+function getFrame() {
+  return document.getElementById("mainFrame").contentWindow;
 }
 
-function frameChanged(url)
-{
-	const s = url.toString();
-	if(s.endsWith("home.html") || s.endsWith("connection.html"))
-	{
-		disconnect();
-	}
+function frameChanged(url) {
+  const s = url.toString();
+  if (s.endsWith("home.html") || s.endsWith("connection.html")) {
+    disconnect();
+  }
 }
-
-
 
 /*------ TCP message handling ------*/
 
@@ -31,113 +24,98 @@ var mainImg;
 
 var messageCache = {};
 
-function provideHandler()
-{
-	return new MessageHandler(messageCache);
+function provideHandler() {
+  return new MessageHandler(messageCache);
 }
 
 const parser = new MessageParser();
-parser.on("message", (id, data) =>
-{
-	console.log("msg", id, data.length > 512 ? "Data too long" : data.toString());
+parser.on("message", (id, data) => {
+  console.log("msg", id, data.length > 512 ? "Data too long" : data.toString());
 
-	const frame = getFrame();
+  const frame = getFrame();
 
-	// special case FIXME: bad
-	if(id === 4)
-	{
-		mainImg = PNG.sync.read(data);
-		frame.location.href = "battle/battle.html";
-	}
+  // special case FIXME: bad
+  if (id === 4) {
+    mainImg = PNG.sync.read(data);
+    frame.location.href = "battle/battle.html";
+  }
 
-	const handler = frame.handler;
+  const handler = frame.handler;
 
-	console.log(handler);
+  console.log(handler);
 
-	if(!handler || !handler.handle(id, data))
-	{
-		if(!messageCache[id])
-		{
-			messageCache[id] = [];
-		}
+  if (!handler || !handler.handle(id, data)) {
+    if (!messageCache[id]) {
+      messageCache[id] = [];
+    }
 
-		messageCache[id].push(data);
-	}
+    messageCache[id].push(data);
+  }
 });
-
-
 
 /*------ TCP connection ------*/
 
 var socket;
 
-function connect(host, port, username)
-{
-	if(!socket)
-	{
-		socket = new Socket();
-		socket.on("data", parser.pipe.bind(parser));
-	}
-	
-	socket.connect({ host, port }, () => sendJson(0, { username }));
+function connect(host, port, username) {
+  if (!socket) {
+    socket = new Socket();
+    socket.on("data", parser.pipe.bind(parser));
+  }
+
+  socket.connect({ host, port }, () => sendJson(0, { username }));
 }
 
-function disconnect()
-{
-	if(socket)
-	{
-		socket.destroy();
-	}
+function disconnect() {
+  if (socket) {
+    socket.destroy();
+  }
 }
-
-
 
 /*------ Broadcast connection ------*/
 
-var broadcastMessage = Buffer.from('Server?');
-var broadcastSocket = Datagram.createSocket('udp4');
+var broadcastMessage = Buffer.from("Server?");
+var broadcastSocket = Datagram.createSocket("udp4");
 
 broadcastSocket.on("listening", () => broadcastSocket.setBroadcast(true));
 
-broadcastSocket.on("message", (message, remote) =>
-{
-	const handler = getFrame().broadcastResponse;
+broadcastSocket.on("message", (message, remote) => {
+  const handler = getFrame().broadcastResponse;
 
-	if(handler)
-	{
-		handler(message, remote);
-	}
+  if (handler) {
+    handler(message, remote);
+  }
 });
 
-broadcastSocket.bind(457);
+broadcastSocket.bind(3000);
 
-function broadcastJson()
-{
-	broadcastSocket.send(broadcastMessage, 0, broadcastMessage.length, 2914, BroadcastAddresses.get()[0]); // TODO: port
+function broadcastJson() {
+  broadcastSocket.send(
+    broadcastMessage,
+    0,
+    broadcastMessage.length,
+    2914,
+    BroadcastAddresses.get()[0]
+  ); // TODO: port
 }
-
-
 
 /*------ TCP message sending ------*/
 
-function send(id, data)
-{
-	if(!socket)
-	{
-		return;
-	}
+function send(id, data) {
+  if (!socket) {
+    return;
+  }
 
-	const idBuffer = Buffer.alloc(1);
-	idBuffer[0] = id;
+  const idBuffer = Buffer.alloc(1);
+  idBuffer[0] = id;
 
-	const dataLength = Buffer.alloc(2);
-	dataLength.writeInt16BE(data.length);
+  const dataLength = Buffer.alloc(2);
+  dataLength.writeInt16BE(data.length);
 
-	const finalData = Buffer.concat([idBuffer, dataLength, data]);
-	socket.write(finalData);
+  const finalData = Buffer.concat([idBuffer, dataLength, data]);
+  socket.write(finalData);
 }
 
-function sendJson(id, obj)
-{
-	send(id, Buffer.from(JSON.stringify(obj)));
+function sendJson(id, obj) {
+  send(id, Buffer.from(JSON.stringify(obj)));
 }
